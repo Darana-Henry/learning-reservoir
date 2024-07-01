27544168050efa4eac92abc7df112e1ab6bdff24
Henry Darana <h.darana@gmail.com>
2024-07-01 11:07:38 +0530

    Reduce, Method Chaining

--- learn-javascript/02-arrays/03-map-filter-and-reduce.js
+++ learn-javascript/02-arrays/03-map-filter-and-reduce.js
@@ -1,4 +1,4 @@
-const transactions = [100, -50, 100, -70, 100, 100, -200, -40];
+const transactions = [100, -50, 100, -70, 100, 100, -200, -40, 150];
 const rateUSD2INR = 83.36;
 
 console.log("--------------------------");
@@ -26,3 +26,74 @@ const detailedTransactions = transactions.map((transaction, index) => {
 });
 
 console.log(detailedTransactions);
+
+console.log("--------------------------");
+console.log("Filter Method");
+console.log("--------------------------");
+//Filter Method
+
+const deposits = transactions.filter(function (transaction) {
+  return transaction > 0;
+});
+
+console.log(`All the deposits ${deposits}`);
+
+const withdrawls = transactions.filter(function (transaction) {
+  return transaction < 0;
+});
+
+console.log(`All the withdrawls ${withdrawls}`);
+
+console.log("--------------------------");
+console.log("Reduce Method");
+console.log("--------------------------");
+//Reduce Method
+
+const balance = transactions.reduce(function (
+  total,
+  transaction,
+  index,
+  transactions
+) {
+  return total + transaction;
+},
+0);
+
+console.log(`Your balance is: ${balance}`);
+
+const largestDeposit = transactions.reduce(function (
+  total,
+  transaction,
+  index,
+  transactions
+) {
+  return total > transaction ? total : transaction;
+  a;
+},
+0);
+
+console.log(`Your largest deposit is: ${largestDeposit}`);
+
+const largestWithdrawal = transactions.reduce(function (
+  total,
+  transaction,
+  index,
+  transactions
+) {
+  return total < transaction ? total : transaction;
+  a;
+},
+0);
+
+console.log(`Your largest withdrawal is: ${largestWithdrawal}`);
+
+console.log("--------------------------");
+console.log("Method Chaining");
+console.log("--------------------------");
+
+const depositsInINR = transactions
+  .filter((transaction) => transaction > 0)
+  .map((transaction) => transaction * rateUSD2INR)
+  .reduce((deposited, transaction) => deposited + transaction, 0);
+
+console.log(`Your deposit total is: ${depositsInINR}`);
